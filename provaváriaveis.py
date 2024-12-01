5e8d30dc212566e11798f20388d12ffdd8a7488c
Evandro-max <evandrobhering@gmail.com>
2024-12-01 12:51:11 -0300

    Add files via upload

---
+++ provaváriaveis.py
@@ -0,0 +1,4 @@
+base = float(input("Qual a base do retângulo?(em centimetros) "))
+altura = float(input("Qual a altura do retângulo?(em centimetros) "))
+resultado = altura * base
+print(f"Sua área é de {resultado}cm")
